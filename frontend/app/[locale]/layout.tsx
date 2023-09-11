d240e85a8df62a390b473d0e8ab555d923d1eaa6
Reyn Alpha <reynalpha@outlook.com>
2023-09-11 17:30:30 +0000

    Import Roboto Mono font

--- frontend/app/[locale]/layout.tsx
+++ frontend/app/[locale]/layout.tsx
@@ -3,6 +3,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import '@fontsource/roboto-mono';
 
 export const metadata: Metadata = {
   title: 'Pixeltracking Web App',
